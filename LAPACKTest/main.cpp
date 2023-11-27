779dc362d602cd2fca3a26beb1b3958a2ebe310f
Matt Hurliman <drgmtt3@gmail.com>
2023-11-27 10:34:11 -0800

    Sussing out the best interface for using lapack

--- LAPACKTest/main.cpp
+++ LAPACKTest/main.cpp
@@ -2,9 +2,9 @@
 #include <cstdlib>
 #include <stdio.h>
 
-#include "lapack.h"
+#include <lapack.h>
 
-int main(int argc, const char** argv)
+void test_dgesv()
 {
     double A[9] = {76, 27, 18, 25, 89, 60, 11, 51, 32};
     double b[3] = {10, 7, 43};
@@ -22,6 +22,65 @@ int main(int argc, const char** argv)
 	printf("The solution is %lf %lf %lf\n", b[0], b[1], b[2]);
     else
 	fprintf(stderr, "dgesv_ fails %d\n", info);
+}
+
+void test_zheev()
+{
+    std::complex<double> A[9];
+
+    A[0] = {1, 0};  A[3] = {2, 0};  A[6] = {3, 0};
+    A[1] = {2, 0};  A[4] = {0, 0};  A[7] = {-3, 0};
+    A[2] = {3, 0};  A[5] = {-3, 0}; A[8] = {3, 0};
+
+    constexpr int N = 3;
+    int lda = 3;
+    int info;
+
+    const int lwork = 2*N-1;
+    double W[N];
+    std::complex<double> work[lwork];
+    double rwork[3*N-2];
+
+    zheev_("N", "U", &N, A, &lda, W, work, &lwork, rwork, &info);
+
+    if(info == 0) /* succeed */
+	printf("The solution is %lf %lf %lf\n", W[0], W[1], W[2]);
+    else
+	fprintf(stderr, "zheev_ fails %d\n", info);
+}
+
+void test_dgeev()
+{
+    double A[9];
+    double b[3] = {10, 7, 43};
+
+    A[0] = 1; A[3] = 2; A[6] = 3;
+    A[1] = -1; A[4] = 0; A[7] = -3;
+    A[2] = 0; A[5] = -2; A[8] = 3;
+
+    constexpr int N = 3;
+    int nrhs = 1;
+    int lda = 3;
+    int ldvl = 1;
+    int ldvr = 1;
+    int info;
+
+    const int lwork = 3*N;
+    double work[lwork];
+    double wr[N];
+    double wi[N];
+
+    dgeev_("N", "N", &N, A, &lda, wr, wi, NULL, &ldvl, NULL, &ldvr, work, &lwork, &info);
+
+    if(info == 0) /* succeed */
+	printf("The solution is %lf %lf %lf\n", wi[0], wi[1], wi[2]);
+    else
+	fprintf(stderr, "zheev_ fails %d\n", info);
+}
+
+int main(int argc, const char** argv)
+{
+    test_zheev();
 
     return 0;
 }
--- qrfactor/main.cpp
+++ qrfactor/main.cpp
@@ -10,6 +10,11 @@
 #include "MatrixMath.h"
 #include "MemoryArena.h"
 
+void QRIterate()
+{
+
+}
+
 int main(int argc, char* argv[])
 {
     using T = double;
@@ -26,23 +31,51 @@ int main(int argc, char* argv[])
     T* RQ = arena.Allocate<T>(N * N);
 
     A[0] = 1; A[3] = 2; A[6] = 3;
-    A[1] =-1; A[4] = 0; A[7] =-3;
-    A[2] = 0; A[5] =-2; A[8] = 3;
+    A[1] = 2; A[4] = 0; A[7] =-3;
+    A[2] = 3; A[5] =-3; A[8] = 3;
 
     Transpose(At, A, N);
-    MatMul(AAt, A, At, N);
-
-    PrintMatrix(AAt, N);
 
     // QR Iteration test
     PrintMatrix(A, N);
-    for (int i = 0; i < 100; ++i)
+    for (int i = 0; i < 20; ++i)
     {
         QRDecompose_Householder(Q, R, A, N);
         MatMul(A, R, Q, N);
     }
-
     PrintMatrix(A, N);
+
+    Transpose(A, At, N);
+    for (int i = 0; i < 20; ++i)
+    {
+        QRDecompose_GramSchmidt(Q, R, A, N);
+        MatMul(A, R, Q, N);
+    }
+    PrintMatrix(A, N);
+    
+    // Iterate -1 Dimension
+    const int M = N - 1;
+    A[0] = 1; A[2] = 2;
+    A[1] = 2; A[3] = 0;
+
+    Transpose(At, A, M);
+
+    // QR Iteration test
+    PrintMatrix(A, M);
+    for (int i = 0; i < 20; ++i)
+    {
+        QRDecompose_Householder(Q, R, A, M);
+        MatMul(A, R, Q, M);
+    }
+    PrintMatrix(A, M);
+
+    Transpose(A, At, M);
+    for (int i = 0; i < 20; ++i)
+    {
+        QRDecompose_GramSchmidt(Q, R, A, M);
+        MatMul(A, R, Q, M);
+    }
+    PrintMatrix(A, M);
 
     return 0;
 }
