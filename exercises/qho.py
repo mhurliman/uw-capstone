f216a20dd83de6111e2d7f4df8714837c8f520d2
Matt Hurliman <drgmtt3@gmail.com>
2024-03-13 17:14:32 -0700

    2D qho

--- exercises/qho.py
+++ exercises/qho.py
@@ -1,6 +1,10 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as anim
+from matplotlib import (
+    pyplot as plt,
+    animation as anim,
+    axes,
+    cm
+)
 
 # Creates a tridiagonal matrix with a, b, c as the repeated elements along the diagonal
 def triagonal(a, b, c, n):
@@ -8,27 +12,41 @@ def triagonal(a, b, c, n):
 
 def qho1d():
     N = 1000
-    L = 50
-    a = L / N
-    h = 1
 
-    w = 2e-2
-    m = 1#9.109e-31 # kg
-    hb = 1#1.054571817e-34 # J s 
+    L = 5
+    a =  2 * L / N
 
-    c = 10 # Energy levels to map
+    f = 8.88e13
+    w = 2 * np.pi * f
 
-    x = np.linspace(-L, L, N+1)
+    m = 9.109e-31 # kg
+    hb = 1.054571817e-34 # J s 
+    hbEv = 6.5821220e-16 # Ev s
+
+    c = 3 # Energy levels to map
+
+    x = np.linspace(-L, L, N + 1)
 
     U = m * w**2 * x**2
-    T = -hb**2 / (2 * m * a**2) * triagonal(1, -2, 1, N+1)
+    T = -hbEv**2 / (2 * m * a**2) * triagonal(1, -2, 1, N + 1)
     H = T + np.diag(U)
 
     e, v = np.linalg.eigh(H)
 
+    vc = np.reshape(v[:, 0], N + 1)
+    ec = e[0]
+    m = np.max(abs(vc) + ec)
+
+    print(e[1] - e[0])
+    print(ec)
+
     fig, ax = plt.subplots()
-    wave = ax.plot(x, np.zeros([N+1, c]))
-    ax.set(ylim=[-1, 1], xlabel='x (m)', ylabel='psi(x, t)')
+    wave = ax.plot(x, np.zeros([N+1, c+1]))
+    ax.set(ylim=[-m, m], xlabel='x (m)', ylabel='psi(x, t)')
+
+    h = 1.0 / 33 / ec * 2 * np.pi
+
+    wave[c].set_ydata(U)
 
     def update(frame):
         t = frame * h
@@ -37,38 +55,85 @@ def qho1d():
         y = np.real(v[:, :c] * np.exp(1j * e[:c] * t)) + e[:c]
 
         for i, w in enumerate(wave):
-            w.set_ydata(y[:, i])
+            if i < c:
+                w.set_ydata(y[:, i])
 
     an = anim.FuncAnimation(fig=fig, func=update, interval=1)
-
     plt.show()
+
 
 def qho2d():
-    N = 1000
-    L = 3
+    N = 40
+    L = 1e-8
+    a =  L / N
 
-    w = 1e-4
-    m = 1 #9.109e-31 # kg
-    hb = 1 #1.054571817e-34 # J s 
+    f = 8.88e13
+    w = 2 * np.pi * f
 
-    psi = lambda x, n: np.exp(1j * np.pi * x)
+    m = 9.109e-31 # kg
+    hb = 1.054571817e-34 # J s 
+    hbEv = 6.5821220e-16 # Ev s
 
-    U = lambda x : m * w**2 * x**2
-    a = -hb**2 / (2 * m)
+    c = 2 # Energy levels to map
+    f = lambda p : -L / 2 + p * a
 
-    T = a * triagonal(1, -2, 1, N + 1)
-    x = np.linspace(-L, L, N + 1)
+    plot_args = {
+        'rstride': 1, 'cstride': 1, 'cmap': 'viridis', 'linewidth': 0.01, 
+        'antialiased': True, 'color': 'w', 'shade': True 
+    }
 
-    H = T + np.diag(U(x))
+    xi, yi = np.arange(N + 1), np.arange(N + 1)
 
+    xis = np.tile(xi, N + 1)
+    yis = np.repeat(yi, N + 1)
+
+    # Fancy method of laying out T
+    xis0, xis1 = np.meshgrid(xis, xis)
+    xx = xis0 - xis1
+    x0, x1 = xx == 0, abs(xx) == 1
+
+    yis0, yis1 = np.meshgrid(yis, yis)
+    yy = yis0 - yis1
+    y0, y1 = yy == 0, abs(yy) == 1
+
+    T = -hbEv**2 / (2 * m * a**2) * np.select([x0 & y0, (x1 & y0) | (x0 & y1)], [-4, 1], 0)
+
+    xs, ys = f(xis), f(yis)
+    U = m * w**2 * (xs**2 + ys**2)
+
+    H = T + np.diag(U)
     e, v = np.linalg.eigh(H)
 
-    for i in range(3):
-        psiv = v[:, i] + e[i] * np.ones_like(x)
-        plt.plot(x, psiv)
-        
+    vc = np.reshape(v[:, c], (N + 1, N + 1))
+    ec = e[c]
+    m = np.max(abs(vc) + ec)
 
+    print(e[:10])
+    print(e[1] - e[0])
+
+    x, y = f(xi), f(yi)
+    xis, yis = np.meshgrid(x, y)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+
+    ax.plot_surface(xis, yis, np.real(vc) + ec, **plot_args)
+
+    h = 1.0 / 33 / ec * 2 * np.pi
+
+    def update(frame):
+        t = frame * h
+
+        ax.clear()
+        ax.set(zlim=[-m, m], xlabel='x (m)', ylabel='y (m)', zlabel='psi(x, t)', title='t = {} (s)'.format(t))
+
+        ax.plot_surface(xis, yis, np.real(vc * np.exp(1j * ec * t)) + ec, **plot_args)
+
+
+    an = anim.FuncAnimation(fig=fig, func=update, interval=33)
     plt.show()
+
+
 
 if __name__ == '__main__':
     qho1d()
