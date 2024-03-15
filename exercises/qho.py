8f75986e760d04e8bd9a028887ed6c8cbc14f8f0
Matt Hurliman <drgmtt3@gmail.com>
2024-03-15 00:51:37 -0700

    Fixed issues with incorrect qho energies

--- exercises/qho.py
+++ exercises/qho.py
@@ -13,46 +13,43 @@ def triagonal(a, b, c, n):
 def qho1d():
     N = 1000
 
-    L = 5
-    a =  2 * L / N
+    xmax = 6
+    dx =  2 * xmax / N
 
     f = 8.88e13
     w = 2 * np.pi * f
 
-    m = 9.109e-31 # kg
-    hb = 1.054571817e-34 # J s 
+    m =  9.109e-31 # kg
+    hb = 1.0 # 1.054571817e-34 # J s 
     hbEv = 6.5821220e-16 # Ev s
 
     c = 3 # Energy levels to map
 
-    x = np.linspace(-L, L, N + 1)
+    xi = np.arange(N)
+    x = np.linspace(-xmax, xmax, N+1)
 
-    U = m * w**2 * x**2
-    T = -hbEv**2 / (2 * m * a**2) * triagonal(1, -2, 1, N + 1)
+    U = 0.5 * m * w**2 * x**2
+    T = -hbEv**2 / (2 * m * dx**2) * triagonal(1, -2, 1, N+1)
     H = T + np.diag(U)
 
     e, v = np.linalg.eigh(H)
 
-    vc = np.reshape(v[:, 0], N + 1)
-    ec = e[0]
-    m = np.max(abs(vc) + ec)
-
-    print(e[1] - e[0])
-    print(ec)
+    vc = np.reshape(v[:, c], N+1)
+    m = np.max(abs(vc) + e[c])
 
     fig, ax = plt.subplots()
     wave = ax.plot(x, np.zeros([N+1, c+1]))
-    ax.set(ylim=[-m, m], xlabel='x (m)', ylabel='psi(x, t)')
+    ax.set(ylim=[0, m], xlabel='x (m)', ylabel='psi(x, t)')
 
-    h = 1.0 / 33 / ec * 2 * np.pi
+    dt = (1.0 / 33) / e[0] * np.pi * 2
 
     wave[c].set_ydata(U)
 
     def update(frame):
-        t = frame * h
+        t = frame * dt
         ax.set(title='t = {}'.format(t))
 
-        y = np.real(v[:, :c] * np.exp(1j * e[:c] * t)) + e[:c]
+        y = np.real(v[:, :c] * np.exp(-1j * e[:c] * t) + e[:c])
 
         for i, w in enumerate(wave):
             if i < c:
@@ -63,24 +60,20 @@ def qho1d():
 
 
 def qho2d():
-    N = 40
-    L = 1e-8
-    a =  L / N
+    N = 50
+
+    xmax = 6
+    dx =  2 * xmax / N
 
     f = 8.88e13
-    w = 2 * np.pi * f
+    w = 1.0 #2 * np.pi * f
 
-    m = 9.109e-31 # kg
-    hb = 1.054571817e-34 # J s 
+    m = 1.0 #9.109e-31 # kg
+    hb = 1.0 #1.054571817e-34 # J s 
     hbEv = 6.5821220e-16 # Ev s
 
     c = 2 # Energy levels to map
-    f = lambda p : -L / 2 + p * a
-
-    plot_args = {
-        'rstride': 1, 'cstride': 1, 'cmap': 'viridis', 'linewidth': 0.01, 
-        'antialiased': True, 'color': 'w', 'shade': True 
-    }
+    f = lambda p : -xmax + p * dx
 
     xi, yi = np.arange(N + 1), np.arange(N + 1)
 
@@ -96,20 +89,20 @@ def qho2d():
     yy = yis0 - yis1
     y0, y1 = yy == 0, abs(yy) == 1
 
-    T = -hbEv**2 / (2 * m * a**2) * np.select([x0 & y0, (x1 & y0) | (x0 & y1)], [-4, 1], 0)
+    T = -hb**2 / (2 * m * dx**2) * np.select([x0 & y0, (x1 & y0) | (x0 & y1)], [-4, 1], 0)
 
     xs, ys = f(xis), f(yis)
-    U = m * w**2 * (xs**2 + ys**2)
+    U = 0.5 * m * w**2 * (xs**2 + ys**2)
 
     H = T + np.diag(U)
     e, v = np.linalg.eigh(H)
 
     vc = np.reshape(v[:, c], (N + 1, N + 1))
     ec = e[c]
-    m = np.max(abs(vc) + ec)
+    m = np.max(abs(vc))
 
-    print(e[:10])
     print(e[1] - e[0])
+    print(e[:10])
 
     x, y = f(xi), f(yi)
     xis, yis = np.meshgrid(x, y)
@@ -117,42 +110,36 @@ def qho2d():
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
 
-    ax.plot_surface(xis, yis, np.real(vc) + ec, **plot_args)
+    ax.plot_surface(xis, yis, np.real(vc) + ec, cmap='viridis')
 
-    h = 1.0 / 33 / ec * 2 * np.pi
+    dt = (1.0 / 33) / ec * 2 * np.pi
 
     def update(frame):
-        t = frame * h
+        t = frame * dt
 
         ax.clear()
-        ax.set(zlim=[-m, m], xlabel='x (m)', ylabel='y (m)', zlabel='psi(x, t)', title='t = {} (s)'.format(t))
+        ax.set(zlim=[ec-m, ec+m], xlabel='x (m)', ylabel='y (m)', zlabel='psi(x, t)', title='t = {} (s)'.format(t))
 
-        ax.plot_surface(xis, yis, np.real(vc * np.exp(1j * ec * t)) + ec, **plot_args)
+        ax.plot_surface(xis, yis, np.real(vc * np.exp(-1j * ec * t)) + ec, cmap='viridis')
 
 
     an = anim.FuncAnimation(fig=fig, func=update, interval=33)
     plt.show()
 
 def qho3d():
-    
-    c = 4 # Energy levels to map
-
-    N = 8
-    L = 1e-4
-    a =  L / N
+    N = 12
+    xmax = 2.5
+    dx =  2 * xmax / N
 
     f = 8.88e13
-    w = 2 * np.pi * f
+    w = 1.0 # 2 * np.pi * f
 
-    m = 9.109e-31 # kg
-    hb = 1.054571817e-34 # J s 
-    hbEv = 6.5821220e-16 # Ev s
+    m = 1.0 # 9.109e-31 # kg
+    hb = 1.0 # 1.054571817e-34 # J s 
 
-    f = lambda p : -L / 2 + p * a
+    c = 8 # Energy levels to map
 
-    plot_args = {
-        'cmap': 'viridis'
-    }
+    f = lambda p : -xmax + p * dx
 
     xi = np.arange(N + 1)
     yi, zi = np.copy(xi), np.copy(xi)
@@ -174,38 +161,35 @@ def qho3d():
     zz = zis0 - zis1
     z0, z1 = zz == 0, abs(zz) == 1
 
-    T = -hbEv**2 / (2 * m * a**2) * np.select([x0 & y0 & z0, (x1 & y0 & z0) | (x0 & y1 & z0) | (x0 & y0 & z1)], [-6, 1], 0)
+    T = -hb**2 / (2 * m * dx**2) * np.select([x0 & y0 & z0, (x1 & y0 & z0) | (x0 & y1 & z0) | (x0 & y0 & z1)], [-6, 1], 0)
 
     xs, ys, zs = f(xis), f(yis), f(zis)
-    U = m * w**2 * (xs**2 + ys**2 + zs**2)
+    U = 0.5 * m * w**2 * (xs**2 + ys**2 + zs**2)
 
     H = T + np.diag(U)
     e, v = np.linalg.eigh(H)
 
     vc = np.reshape(v[:, c], (N + 1, N + 1, N + 1))
     ec = e[c]
-    m = np.max(abs(vc) + ec)
 
-    print(e[:10])
     print(e[1] - e[0])
-
-    x, y, z = f(xi), f(yi), f(zi)
-    xis, yis, zis = np.meshgrid(x, y, z)
+    print(e[:20])
 
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
 
-    ax.scatter(xis, yis, zis, c=np.real(vc) + ec, **plot_args)
+    xis, yis, zis = np.meshgrid(f(xi), f(yi), f(zi))
+    ax.scatter(xis, yis, zis, c=np.real(vc) + ec, cmap='viridis')
 
-    h = 1.0 / 33 / ec * 2 * np.pi
+    dt = (1.0 / 33) / ec * 2 * np.pi
 
     def update(frame):
-        t = 0 #frame * h
+        t = frame * dt
 
         ax.clear()
         ax.set(xlabel='x (m)', ylabel='y (m)', zlabel='y (m)', title='t = {} (s)'.format(t))
 
-        ax.scatter(xis, yis, zis, c=np.real(vc * np.exp(1j * ec * t)) + ec, **plot_args)
+        ax.scatter(xis, yis, zis, c=np.real(vc * np.exp(-1j * ec * t)) + ec, cmap='viridis')
 
 
     an = anim.FuncAnimation(fig=fig, func=update, interval=33)
